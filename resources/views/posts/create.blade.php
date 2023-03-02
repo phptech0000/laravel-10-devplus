74d470bc7c11a563df423d52cecefc3f9213b31a
lukecode <lucassocorrosilva@gmail.com>
2023-03-02 11:14:28 -0300

    Add estilos

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,13 +4,15 @@
     Criar uma nova publicação
 @endsection
 
+@push('styles')
+<link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+@endpush
 
 @section('content')
     <div class="md:flex md:items-center">
-
         <div class="md:w-1/2 px-10">
-            <form action="/IMAGEMS" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center" id="dropzone">
-
+            <form action="{{route('images.store')}}" method="POST" enctype="multipart/form-data" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center" id="dropzone">
+                @csrf
             </form>
         </div>
 
