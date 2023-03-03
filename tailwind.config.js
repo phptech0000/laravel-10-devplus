2d1efd5cfd1fdc5cf3b203b682a7c3f44b18c2f4
lukecode <lucassocorrosilva@gmail.com>
2023-03-03 18:10:54 -0300

    Passando novas configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    ],
     theme: {
         extend: {},
     },
