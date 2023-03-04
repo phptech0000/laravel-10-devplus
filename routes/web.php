7b09e383f9cb19d65d9404cbee1b2ace1909b6b1
lukecode <lucassocorrosilva@gmail.com>
2023-03-04 17:45:51 -0300

    Add novas rotas

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,9 @@ Route::get('/login',[LoginController::class, 'index'])->name('login');
 Route::post('/login',[LoginController::class, 'store']);
 Route::post('/logout',[LogoutController::class, 'store'])->name('logout');
 
-
 Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/{user:username}/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
 Route::post('/imagens', [ImageController::class, 'store'])->name('images.store');
