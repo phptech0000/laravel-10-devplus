010d5e4eb57334488454d633ecb5a0bcee5c9bce
lukecode <lucassocorrosilva@gmail.com>
2023-03-04 17:46:37 -0300

    Add funcionalidades do dropzone

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import Dropzone from "dropzone";
 
+const DocumentImage = document.querySelector('[name="image"]');
+
 Dropzone.autoDiscover = false;
 
 const dropzone = new Dropzone("#dropzone", {
@@ -9,4 +11,31 @@ const dropzone = new Dropzone("#dropzone", {
     dictRemoveFile: "Excluir arquivo",
     maxFiles: 1,
     uploadMultiple: false,
+
+    init: function () {
+        if (DocumentImage.value.trim()) {
+            const imagePublic = {};
+            imagePublic.size = 1234;
+            imagePublic.name = DocumentImage.value;
+
+            this.options.addedfile.call(this, imagePublic);
+            this.options.thumbnail.call(
+                this,
+                imagePublic,
+                `/uploads/${imagePublic.name}`
+            );
+            imagePublic.previewElement.classList.add(
+                "dz-success",
+                "dz-complete"
+            );
+        }
+    },
+});
+
+dropzone.on("success", function (file, response) {
+    DocumentImage.value = response.image;
+});
+
+dropzone.on("removedfile", function () {
+    DocumentImage.value = "";
 });
