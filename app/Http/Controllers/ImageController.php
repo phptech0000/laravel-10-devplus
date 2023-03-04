b4c17f558b94cace5e7eec4986b97e23aff9f2f0
lukecode <lucassocorrosilva@gmail.com>
2023-03-04 19:30:20 -0300

    Add novas config

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImageController extends Controller
 {
-    public function store(){
+    public function store(Request $request)
+    {
 
+        $image = $request->file('file');
+
+        $nameImage = Str::uuid() . '.' . $image->extension();
+
+        $imageServer = Image::make($image);
+        $imageServer->fit(1000, 1000);
+
+        $imagePath = public_path('uploads') . '/' . $nameImage;
+        $imageServer->save($imagePath);
+
+        return response()->json(['image' => $nameImage]);
     }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -15,8 +16,13 @@ class PostController extends Controller
 
  public function index(User $user)
  {
+
+    $posts = Post::where('user_id', $user->id)->paginate(5);
+
+
     return view('layouts.dashboard', [
-        'user' => $user
+        'user' => $user,
+        'posts' => $posts
     ]);
  }
 
@@ -24,5 +30,34 @@ class PostController extends Controller
  {
     return view('posts.create');
  }
+
+ public function store(Request $request)
+ {
+    $this->validate($request, [
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'image' => 'required'
+    ]);
+
+    $request->user()->posts()->create([
+        'title' => $request->title,
+        'description' => $request->description,
+        'image' => $request->image,
+        'user_id' => auth()->user()->id
+    ]);
+
+    return redirect()->route('posts.index', auth()->user()->username);
+
+    }
+
+
+    public function show(User $user, Post $post)
+    {
+
+        return view('posts.show', [
+            'post' => $post
+        ]);
+
+    }
 
 }
