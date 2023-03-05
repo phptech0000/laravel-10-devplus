801e72c8045e9b26115e4a9166b54267d1cc914f
lukecode <lucassocorrosilva@gmail.com>
2023-03-05 20:54:57 -0300

    Criando rota para comentarios

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
-use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+use App\Http\Controllers\ImageController;
 use App\Http\Controllers\LoginController;
+
 use App\Http\Controllers\LogoutController;
 
-use App\Http\Controllers\ImageController;
 
-
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\RegisterController;
 
 
 /*
@@ -38,5 +39,7 @@ Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.in
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/{user:username}/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+Route::post('/{user:username}/posts/{post}', [CommentController::class, 'store'])->name('comments.store');
 
 Route::post('/imagens', [ImageController::class, 'store'])->name('images.store');
