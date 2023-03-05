2f252d99800b05c2271b00d7a0f4ad7a34710a15
lukecode <lucassocorrosilva@gmail.com>
2023-03-05 19:43:24 -0300

    Add route protection

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except(['show', 'index']);
     }
 
  public function index(User $user)
