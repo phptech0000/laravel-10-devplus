dd8ec9375d560627f834d6b556dd7f77f6b288d6
lukecode <lucassocorrosilva@gmail.com>
2023-03-06 20:09:16 -0300

    routa de delete criada

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,8 @@ Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.in
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/{user:username}/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::delete('/posts/{post}', [PostController::class,'destroy'])->name('posts.destroy');
+
 
 Route::post('/{user:username}/posts/{post}', [CommentController::class, 'store'])->name('comments.store');
 
