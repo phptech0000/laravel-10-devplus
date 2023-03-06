13982affd33cd6a67295ff6af7b7d6dae4441ca4
lukecode <lucassocorrosilva@gmail.com>
2023-03-06 20:10:15 -0300

    Criando politica para função

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PostPolicy
+{
+    public function delete(User $user, Post $post): bool
+    {
+        return $user->id === $post->user_id;
+    }
+}
