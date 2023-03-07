e9c28169111909dda8dd698451ea15a43f6daf6e
lukecode <lucassocorrosilva@gmail.com>
2023-03-07 20:46:01 -0300

    Criando função

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class LikeController extends Controller
 {
-    //
+    public function store(Request $request, Post $post)
+    {
+        $post->likes()->create([
+            'user_id' => $request->user()->id
+        ]);
+
+        return back();
+    }
 }
