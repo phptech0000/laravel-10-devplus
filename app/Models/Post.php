05987ed64190056b2073788018624b8c9609cef6
lukecode <lucassocorrosilva@gmail.com>
2023-03-08 19:13:01 -0300

    Criando função para checar like

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -27,4 +27,14 @@ class Post extends Model
         return $this->hasMany(Comment::class);
     }
 
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function checkLike(User $user)
+    {
+        return $this->likes->contains('user_id', $user->id);
+    }
+
 }
