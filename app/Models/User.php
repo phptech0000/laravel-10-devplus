5830bc5b31ac60ea7bd43ba1aa39aaa5c46f1961
lukecode <lucassocorrosilva@gmail.com>
2023-03-08 19:12:16 -0300

    Criando para função para like

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
     }
 
+    public function Likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
 }
