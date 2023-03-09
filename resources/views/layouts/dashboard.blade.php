f24c3f9e073cc418bab362d17a55a5fa4e59cdbc
lukecode <lucassocorrosilva@gmail.com>
2023-03-09 18:52:41 -0300

    Add imagem de perfil

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -10,11 +10,30 @@
     <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
             <div class="w-8/12 lg:w-6/12 px-5">
-                <img src="{{ asset('img/user.svg') }}" alt="user">
+                <img class="rounded-full"
+                    src="{{ $user->image ? asset('perfiles') . '/' . $user->image : asset('img/user.svg') }}" alt="user">
             </div>
             <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py-10 md:py-10">
-                <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
+                <div class="flex items-center gap-4">
+                    <p class="text-gray-700 text-2xl capitalize">{{ $user->username }}</p>
 
+                    @auth
+
+                        @if ($user->id === auth()->user()->id)
+                            <a class="text-violet-500 hover:text-violet-600 transition-colors cursor-poinet"
+                                href="{{ route('perfil.index') }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                </svg>
+
+                            </a>
+                        @endif
+
+                    @endauth
+
+                </div>
                 <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
                     0
                     <span class="font-normal">Seguidores</span>
