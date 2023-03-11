725c176cfdd25099c71589f7d84c198094b300e5
lukecode <lucassocorrosilva@gmail.com>
2023-03-11 17:36:21 -0300

    Criando funções para mostrar contagem de seguidores

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,21 @@ class User extends Authenticatable
         return $this->hasMany(Like::class);
     }
 
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+
+    public function following(User $user)
+    {
+        return $this->followers->contains($user->id);
+    }
+
+
 }
