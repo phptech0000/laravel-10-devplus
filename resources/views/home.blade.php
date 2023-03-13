bfa2a7a89d659948227a029da3a71bb0c189ab02
lukecode <lucassocorrosilva@gmail.com>
2023-03-13 18:15:58 -0300

    Add novos estilos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,22 @@
 @endsection
 
 @section('content')
-    Conteudo da pagina
+    @if ($posts->count())
+        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 ">
+            @foreach ($posts as $post)
+                <div class="">
+                    <a href="{{ route('posts.show', ['post' => $post, 'user' => $post->user]) }}">
+                        <img src="{{ asset('uploads') . '/' . $post->image }}" alt="Imagem de post {{ $post->title }}">
+                    </a>
+                </div>
+            @endforeach
+        </div>
+
+        <div class="my-10">
+            {{ $posts->links('pagination::tailwind') }}
+        </div>
+    @else
+        <p class="text-center">Nao a posts, siga alguem para poder ver os posts</p>
+    @endif
+
 @endsection
