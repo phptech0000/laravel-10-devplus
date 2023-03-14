69a399ffed75472de9bab6d043021d8d0e554272
lukecode <lucassocorrosilva@gmail.com>
2023-03-14 17:49:55 -0300

    Ajustando rotas

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,6 @@ use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\RegisterController;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', HomeController::class)->name('home');
 
