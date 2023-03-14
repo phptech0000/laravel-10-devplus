e9c1ef994447e9a5a9e5db14802eea4e5b9d135f
lukecode <lucassocorrosilva@gmail.com>
2023-03-14 18:38:22 -0300

    Corrigindo a função

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if($this->app>environment('production'))
+        if($this->app->environment('production'))
+
         {
             URL::forceScheme('https');
         }
